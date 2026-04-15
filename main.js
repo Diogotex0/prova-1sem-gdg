f659e79ed66dafca17289baafa9e378938bfc8f7
Diogotex0 <diogoteixeira4000@gmail.com>
2026-04-15 19:38:51 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,5 @@
+/* Desenvolva a lógica do seu To-Do List aqui.
+ Lembre-se:
+ 1. Ao criar o botão de excluir, adicione a classe: 'btn-remover'
+ 2. Ao criar o botão/checkbox de concluir, adicione a classe: 'btn-concluir'
+ */
