663e415ca9ccfcf8c5c82a7c59d44259e3146a17
Diogotex0 <diogoteixeira4000@gmail.com>
2026-04-15 19:46:05 -0300

    feat(js): adiciona array de tarefas e função adicionarTarefa

--- main.js
+++ main.js
@@ -3,3 +3,21 @@
  1. Ao criar o botão de excluir, adicione a classe: 'btn-remover'
  2. Ao criar o botão/checkbox de concluir, adicione a classe: 'btn-concluir'
  */
+
+// Array global para armazenar as tarefas
+var dadosTarefas = [];
+
+// Função para adicionar tarefa
+function adicionarTarefa() {
+    var novaTarefa = document.getElementById('nova-tarefa').value;
+
+    if (novaTarefa) {
+        dadosTarefas.push({ texto: novaTarefa, concluida: false });
+        criaLista();
+        document.getElementById('nova-tarefa').value = "";
+    } else {
+        alert("Favor, preencha o campo de tarefa");
+    }
+}
+
+document.getElementById('btn-adicionar').addEventListener('click', adicionarTarefa);
